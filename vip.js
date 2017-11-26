1e40bca9c44abcc154ac6863bf389a06228bb54f
qinef <qinef1@gmail.com>
2017-11-26 17:27:02 +0800

    Update vip.js

--- vip.js
+++ vip.js
@@ -5,7 +5,7 @@
 |http://www.tudou.com/albumplay/*.html$s@</body>@<script>insVIP('tab_nav fix','player');</script></body>@
 |http://www.tudou.com/albumplay/*.html##.tab_nav.fix select{margin-top:11px;font-size: 15px}csshack
 
-|http://www.iqiyi.com/v_*$s@</head>@<script src="https://github.com/qinef/koolproxy-vip/blob/master/vip_iqiyi.js"></script></head>@
+|http://www.iqiyi.com/v_*$s@</head>@<script src="https://github.com/qinef/koolproxy-vip/blob/master/vip.js"></script></head>@
 |http://www.iqiyi.com/v_*$s@</body>@<script>insVIP('mod-play-tit','flashbox');</script></body>@
 |http://www.iqiyi.com/v_*##div[style*="absolute"][style*="visibility"]
 |http://www.le.com/$s@</head>@<script src="https://github.com/qinef/koolproxy-vip/blob/master/vip.js"></script></head>@
@@ -14,7 +14,7 @@
 ||https://sohu.com/album/$s@</head>@<script src="https://github.com/qinef/koolproxy-vip/blob/master/vip.js"></script></head>@
 ||https://sohu.com/album/$s@</body>@<script>insVIP('player-content-info','playerWrap');</script></body>@
 ||https://sohu.com/album/##.player-content-info>select{margin-top:20px;font-size: 15px}csshack
-|https://v.qq.com/x/*.html*$s@</head>@<script src="https://github.com/qinef/koolproxy-vip/blob/master/vip_qq.js"></script></head>@
+|https://v.qq.com/x/*.html*$s@</head>@<script src="https://github.com/qinef/koolproxy-vip/blob/master/vip.js"></script></head>@
 |https://v.qq.com/x/*.html*$s@</body>@<script>insVIPssl('action_wrap cf','tenvideo_player');</script></body>@
 |https://v.qq.com/x/*.html*##[class="action_wrap cf"]>select{margin-top:18px}[class="action_item action_count"] .icon_text
 |http://www.mgtv.com/b/*$s@</head>@<script src="https://github.com/qinef/koolproxy-vip/blob/master/vip.js"></script></head>@
